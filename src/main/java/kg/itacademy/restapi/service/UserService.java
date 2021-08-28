384540f48d76dc07f832904df76447d1fc55ff69
Almanbet Totoev <almanbet.totoev@mail.ru>
2021-08-28 20:09:19 +0600

    hackaton

---
+++ src/main/java/kg/itacademy/restapi/service/UserService.java
@@ -0,0 +1,13 @@
+package kg.itacademy.restapi.service;
+
+import kg.itacademy.restapi.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> getAll();
+    User getById(Long id);
+    User save(User user);
+    User deleteById(Long id);
+    User getByEmailPassword(String email, String password);
+}
---
+++ src/main/java/kg/itacademy/restapi/service/impl/UserServiceImpl.java
@@ -0,0 +1,45 @@
+package kg.itacademy.restapi.service.impl;
+
+import kg.itacademy.restapi.entity.User;
+import kg.itacademy.restapi.repository.UserRepository;
+import kg.itacademy.restapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User deleteById(Long id) {
+        User user = getById(id);
+        userRepository.deleteById(id);
+        return user;
+    }
+
+    @Override
+    public User getByEmailPassword(String email, String password) {
+        User user = userRepository.getByEmailAndPassword(email,password);
+        if(user == null){
+            return null;
+        }
+        return user;
+    }
+}
